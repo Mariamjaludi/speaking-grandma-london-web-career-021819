e4f561a27e10eb9c04cbffd7172a32e193d07ff4
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-01 13:39:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN"
+  return "I LOVE YOU TOO PUMPKIN!"
   elsif input == input.uppercase
     return "NO, NOT SINCE 1938!"
   else
